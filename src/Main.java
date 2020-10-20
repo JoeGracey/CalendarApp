5bb6891d76db9b418ede2b694752b50871dfb7ae
Joe Gracey <joe.gracey@gmail.com>
2020-10-19 19:31:44 -0600

    java.util.Calendar + variables

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int selectedYear = 2020;
+        int selectMonth = 2;
+
+        Calendar calendarObject = new GregorianCalendar();
+        int calendarDay = calendarObject.get(Calendar.DATE);
+        int calendarMonth = calendarObject.get(Calendar.MONTH);
+        int calendarYear = calendarObject.get(Calendar.YEAR);
+
+    }
+}
