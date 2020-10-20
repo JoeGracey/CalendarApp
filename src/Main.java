184deed83ed28350e929b68b8a33f670d98df905
Joe Gracey <joe.gracey@gmail.com>
2020-10-19 21:10:15 -0600

    number of days

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,24 @@ public class Main {
         int calendarMonth = calendarObject.get(Calendar.MONTH);
         int calendarYear = calendarObject.get(Calendar.YEAR);
 
+        // Count the months
         GregorianCalendar gregorianCalendarObject = new GregorianCalendar(selectedYear,
                 selectMonth, 1);
         int days = gregorianCalendarObject.getActualMaximum(Calendar.DATE);
         int startInWeek = gregorianCalendarObject.get(Calendar.DAY_OF_WEEK);
 
+        // Count the weeks
         gregorianCalendarObject = new GregorianCalendar(selectedYear, selectMonth, days);
         int totalWeeks = gregorianCalendarObject.getActualMaximum(Calendar.WEEK_OF_MONTH);
 
+        int count = 1; // Count the days
+        for(int i = 1; i <= totalWeeks; i++) {
 
+            for(int j = 1; j <= 7; j++) {
+
+            }
+            System.out.print(i + " ");
+        }
         System.out.println(totalWeeks);
 
     }
